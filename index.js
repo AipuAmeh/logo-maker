c69ccaac3a686bffe314be72c40e217e157b90ed
AipuAmeh <aipuameh.16@gmail.com>
2023-05-23 18:48:21 -0400

    rendered data to all shapes for logo

--- index.js
+++ index.js
@@ -1,42 +1,66 @@
-const inquirer = require('inquirer');
-const fs = require("fs");
-const shapes = require("./lib/circles");
+const inquirer = require("inquirer");
+const fs = require("fs/promises");
+const Triangle = require("./lib/Triangle");
+const Square = require("./lib/Square");
+const Circle = require("./lib/Circle");
+const SVG = require("./lib/Svg")
 
-
-inquirer 
-   .prompt([
-        {
-            type: "input",
-            name: "characters",
-            message: "Please type in 3 characters"
-        },
-        {
-            type: "input",
-            name: "textColor",
-            message: "What text color would you like?",
-        },
-        {
-            type: "list",
-            name: "shape",
-            message: "Choose one of the following shapes",
-            choices: ["Circle", "Triangle", "Square"],
-        },
-        {
-            type: "input",
-            name: "shapeColor",
-            message: "What shape color would you like?"
-        }
-   ])
-   .then((svg) => {
-    // console.log(JSON.stringify(svg));
-    console.log(svg);
-    svg.shape;
-    svg.characters;
-    let newShape = new shapes(this.shape).render(svg)
-    // console.log(newShape);
-  
-    //what data do you want to be written to logo file
-    //call the circle shape class if it is chosen and render it to svg file
-    fs.writeFileSync("logo.svg",newShape, (err) => err ? console.log(err) : console.log("Generated logo.svg"))
-
-   })
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "characters",
+      message: "Please type in 3 characters",
+    },
+    {
+      type: "input",
+      name: "textColor",
+      message: "What text color would you like?",
+    },
+    {
+      type: "list",
+      name: "shape",
+      message: "Choose one of the following shapes",
+      choices: ["Circle", "Triangle", "Square"],
+    },
+    {
+      type: "input",
+      name: "shapeColor",
+      message: "What shape color would you like?",
+    },
+  ])
+  .then(async (svg) => {
+    //if user responds to prompts, enter response in to render method based on the class
+    let newShape;
+    if (svg.shape == "Circle") {
+      newShape = new Circle(
+        svg.characters,
+        svg.shapeColor,
+        svg.textColor
+      );
+    }
+    if (svg.shape == "Triangle") {
+      newShape = new Triangle(
+        svg.characters,
+        svg.shapeColor,
+        svg.textColor
+      );
+    }
+    if (svg.shape == "Square") {
+      newShape = new Square(
+        svg.characters,
+        svg.shapeColor,
+        svg.textColor
+      );
+    }
+    
+    try {
+      console.log(newShape)
+      var svgLogo = new SVG(newShape)
+      console.log(svgLogo)
+      await fs.writeFile("logo.svg", svgLogo.renderSVG());
+      console.log("Generated logo.svg");
+    } catch (error) {
+      console.log(error);
+    }
+  });
